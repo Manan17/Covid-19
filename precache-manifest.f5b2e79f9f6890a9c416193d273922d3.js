f0487b8043f373b39b9b2915582c4d2ab45e8cdf
unknown <shahmanan170602@gmail.com>
2020-06-09 00:48:49 +0530

    Updates

---
+++ precache-manifest.f5b2e79f9f6890a9c416193d273922d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33797508231b0847452c0f8867c5c54e",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "86ed30716e395f96c7f2",
+    "url": "/Covid-19/static/css/main.e6d9fb2b.chunk.css"
+  },
+  {
+    "revision": "3ec426bcb1aada63cb3f",
+    "url": "/Covid-19/static/js/2.f126bbd9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.f126bbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ed30716e395f96c7f2",
+    "url": "/Covid-19/static/js/main.074ae299.chunk.js"
+  },
+  {
+    "revision": "f7f0e2d4ed5b6ad6faec",
+    "url": "/Covid-19/static/js/runtime-main.10a0a340.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19/static/media/corona-image.d7265326.png"
+  }
+]);
